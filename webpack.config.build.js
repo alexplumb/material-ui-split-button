8711974cea18080803bca48b4a4aa6a0e9b54c8d
Alex Plumb <alex@sidedooraccess.com>
2019-04-05 21:39:44 -0300

    Scaffolding

---
+++ webpack.config.build.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const common = require('./webpack.config.common');
+
+const ROOT_PATH = path.resolve(__dirname);
+
+module.exports = merge(common, {
+  mode: 'production',
+  entry: {
+    'material-ui-split-button': path.resolve(ROOT_PATH, 'src/index.js'),
+  },
+  output: {
+    path: path.resolve(ROOT_PATH, 'dist'),
+    filename: 'index.js',
+    library: 'MaterialUiSplitButton',
+    libraryTarget: 'umd',
+  },
+  externals: [
+    {
+      react: {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react',
+      },
+      'react-dom': {
+        root: 'ReactDOM',
+        commonjs2: 'react-dom',
+        commonjs: 'react-dom',
+        amd: 'react-dom',
+      },
+    },
+    "@material-ui/core",
+    /@material-ui\/core\/*./,
+  ],
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+      __DEV__: false,
+    }),
+  ],
+});
---
+++ webpack.config.dev.ts
@@ -0,0 +1,47 @@
+import webpack from 'webpack';
+import webpackMerge from 'webpack-merge';
+import webpackConfigCommon from './webpack.config.common';
+
+const webpackConfig: webpack.Configuration = webpackMerge(webpackConfigCommon, {
+  devtool: 'inline-source-map',
+  devServer: {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
+      'Access-Control-Allow-Headers': 'content-type,authorization,accept',
+    },
+    port: 8181,
+    inline: true,
+    historyApiFallback: true,
+    clientLogLevel: 'none',
+    open: true,
+    contentBase: 'dist',
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('development'),
+      },
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'tslint-loader',
+          options: {
+            fix: true,
+            formatter: 'verbose',
+            emitErrors: true,
+          },
+        },
+      },
+    ],
+  },
+});
+
+export default webpackConfig;
